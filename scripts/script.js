23475477efb5a993e6e798e4f0e27e5a0dddeff2
Diego Ramon <Diegoramon@live.nl>
2024-09-19 12:35:48 +0200

    javascript update

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,35 @@
 // JavaScript Document
-console.log("hi");
+const menuButton = document.querySelector('.menuButton')
+const menuBody = document.querySelector('nav')
+const header = document.querySelector('header')
+
+let headerHeight = header.offsetHeight;
+let lastScrollTop = 0;
+let scrollFromTop;
+
+function openMenu(){
+  if (menuBody.style.display === "grid") {
+    menuBody.style.display = "none";
+  } else {
+    menuBody.style.display = "grid";
+  }
+}
+
+function scrollHide() {
+  scrollFromTop = document.documentElement.scrollTop;
+  if (scrollFromTop > lastScrollTop) {
+    header.classList.remove("header-down");
+    header.classList.add('header-up');
+  } else if(scrollFromTop < 100){
+    header.classList.remove("header-down");
+    header.classList.add('header-up');
+  }
+  else {
+    header.classList.remove('header-up');
+    header.classList.add("header-down");
+  }
+  lastScrollTop = scrollFromTop <= 0 ? 0 : scrollFromTop;
+}
+window.onscroll = function() {scrollHide()};
+
+menuButton.addEventListener('click', openMenu);
